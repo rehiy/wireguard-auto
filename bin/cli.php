cc941f0ba6ff57359c5d5138860f4cef65d7c66d
若海 <wang@rehiy.com>
2022-02-25 13:13:53 +0800

    支持编译为 docker

--- bin/cli.php
+++ bin/cli.php
@@ -1,5 +1,9 @@
 <?php
 
-$wglist = parse_ini_file('config.ini', true);
+if (is_file('config-prod.ini')) {
+    $wglist = parse_ini_file('config-prod.ini', true);
+} else {
+    $wglist = parse_ini_file('config.ini', true);
+}
 
 include 'wg.php';
